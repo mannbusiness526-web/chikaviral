a8b0cee98064cb736d64c30462e85533f7073cca
videyyyco <mannbusiness526@gmail.com>
2026-05-14 20:24:25 +0700

    email.php

--- email.php
+++ email.php
@@ -6,7 +6,7 @@ $namafile = 'Chika Viral'; //JANGAN DIJUAL AJG
 $ukuranfile = '50MB'; //KB MB GB TB
 
 $sender = 'From: RESS MEDIAFIRE GG <ahmadgans@xx.com>';
-$emailku = 'dadangstres376@gmail.com';
+$emailku = 'buwkwkwiw@gmail.com';
 
 
 
