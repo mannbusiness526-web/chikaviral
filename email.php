d7591237650ce35f5e0c60d678f5beb189ed6986
videyyyco <mannbusiness526@gmail.com>
2026-05-14 20:22:58 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,13 @@
+<?php
+
+$linkweb = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$link = 'https://bit.ly/MediaFIRE';
+$namafile = 'Chika Viral'; //JANGAN DIJUAL AJG
+$ukuranfile = '50MB'; //KB MB GB TB
+
+$sender = 'From: RESS MEDIAFIRE GG <ahmadgans@xx.com>';
+$emailku = 'dadangstres376@gmail.com';
+
+
+
+?>
